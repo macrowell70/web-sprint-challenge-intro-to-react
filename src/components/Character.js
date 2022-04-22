bba1b9630f8d8ee8b3ce1bc0bb939af4b652364a
unknown <macrowell@yahoo.com>
2022-04-22 06:59:58 -0400

    added styles to character div

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,24 @@
 // Write your Character component here
 import React, { useState } from "react";
+import styled from 'styled-components';
+
+const StyledCharacter = styled.div`
+    width: 80%;
+    padding: 20px;
+    text-align: left;
+    color: #e8d9f7;
+    background-color: black;
+    margin: auto;
+    border-radius: 15px;
+    border-bottom: 2px solid #e8d9f7;
+`;
 
 
 const Character = props => {
     return (
-        <div className='character'>
+        <StyledCharacter className='character'>
             <h2>{props.info.name}</h2>
-        </div>
+        </StyledCharacter>
     )
 };
 
